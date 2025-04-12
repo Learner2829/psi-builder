08864fcb4f0a504f815df3cb29b802f1770c87ca
ASHISH G PATNI <136146658+Learner2829@users.noreply.github.com>
2025-04-12 10:56:34 +0530

    Api_connection file

--- psi_builder/psi_builder/urls.py
+++ psi_builder/psi_builder/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import Api_connection
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',Api_connection.main,name='main')
 ]
